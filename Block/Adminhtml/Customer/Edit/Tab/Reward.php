e14dfa963ec478322decfabbd8b1aaa908c3ace9
igorpilat <i.pilat@invade.solutions>
2023-02-08 23:02:34 +0200

    add RP tab at customer view in admin panel

---
+++ Block/Adminhtml/Customer/Edit/Tab/Reward.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Piltec\RewardPoints\Block\Adminhtml\Customer\Edit\Tab;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+
+class Reward extends \Magento\Backend\Block\Template implements \Magento\Ui\Component\Layout\Tabs\TabInterface
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'tab/reward_points.phtml';
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCustomerId()
+    {
+        return $this->_coreRegistry->registry(\Magento\Customer\Controller\RegistryConstants::CURRENT_CUSTOMER_ID);
+    }
+
+    /**
+     * @return \Magento\Framework\Phrase
+     */
+    public function getTabLabel()
+    {
+        return __('Reward Points');
+    }
+
+    /**
+     * @return \Magento\Framework\Phrase
+     */
+    public function getTabTitle()
+    {
+        return __('Reward Points');
+    }
+
+    /**
+     * @return bool
+     */
+    public function canShowTab()
+    {
+        if ($this->getCustomerId()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHidden()
+    {
+        if ($this->getCustomerId()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Tab class getter
+     *
+     * @return string
+     */
+    public function getTabClass()
+    {
+        return '';
+    }
+
+    /**
+     * Return URL link to Tab content
+     *
+     * @return string
+     */
+    public function getTabUrl()
+    {
+        return '';
+    }
+
+    /**
+     * Tab should be loaded trough Ajax call
+     *
+     * @return bool
+     */
+    public function isAjaxLoaded()
+    {
+        return false;
+    }
+}
