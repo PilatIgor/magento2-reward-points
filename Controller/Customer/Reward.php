62fde90e19862ddcd4c4f94193fea7857ebffad8
igorpilat <i.pilat@invade.solutions>
2023-02-09 20:59:14 +0200

    create RW customer account tab with current point amount

---
+++ Controller/Customer/Reward.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Piltec\RewardPoints\Controller\Customer;
+
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Reward render RW account tab
+ */
+class Reward implements \Magento\Framework\App\ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        PageFactory $pageFactory
+    ) {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
