3ddbcdb1ed4bb4e5a8f905cd6ec309d8a0dec6f9
igorpilat <i.pilat@invade.solutions>
2023-02-11 23:36:17 +0200

    add rp field po customer grid without functionality

---
+++ Ui/Component/Listing/Column/RewardPoint.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Piltec\RewardPoints\Ui\Component\Listing\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+
+class RewardPoint extends Column
+{
+
+}
